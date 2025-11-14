459a6a22e58684a21dce88ff89cd3946148ef936
J <133714506+J-Feltens@users.noreply.github.com>
2025-11-14 10:39:05 +0100

    got a pretty decent boilerplate ready

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,41 @@
 use image::{DynamicImage, GenericImageView, ImageReader, Pixel, Rgba};
 use minifb::{Key, MouseButton, MouseMode, Window, WindowOptions};
-use rand::{Rng, rng};
+
 use std::process::exit;
-use std::time::{SystemTime, SystemTimeError, UNIX_EPOCH};
-use std::{thread, time};
 
-use crate::canvas::Canvas;
 use crate::colors::{BLACK, CYAN, Color, MAGENTA, TRANSPARENT, WHITE, YELLOW};
-use crate::dictionary::Dictionary;
-use crate::game::Game;
-use crate::letters::Letter;
-use crate::sprites::{Circle, LetterCircle, Sprite, sprite};
-use crate::util::Vector2d;
+use crate::vectors::Vector2d;
 
-pub mod canvas;
 pub mod colors;
-pub mod dictionary;
-pub mod game;
-pub mod letters;
-pub mod sprites;
 pub mod util;
+pub mod vectors;
 
-const TARGET_FPS: u32 = 1000;
+const SIZE_X: u32 = 64;
+const SIZE_Y: u32 = 64;
 
-const SIZE_X: u32 = 1000;
-const SIZE_Y: u32 = 600;
-
-// the quick'n dirty
 fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
-    // fn main() {
-    let mut game = Game::new(SIZE_X, SIZE_Y, TARGET_FPS);
-    game.init();
-    return game.run_game();
+    let mut window = Window::new(
+        "RRP (Rusty Ruling Pen)",
+        SIZE_X as usize,
+        SIZE_Y as usize,
+        WindowOptions::default(),
+    )?;
 
-    // initialize dictionary
-    let mut root: Letter = Letter::new(' ');
-    let dictionary: Dictionary = Dictionary::new();
-    for word in dictionary.dictionary.iter() {
-        root.insert(word.to_string());
+    let mut buffer: Vec<u32> = vec![0; (SIZE_X * SIZE_Y) as usize];
+    for i in 0..(SIZE_X * SIZE_Y) as usize {
+        buffer[i] = Color::as_u32(&Color {
+            r: (255),
+            g: (255),
+            b: (255),
+            a: (1.0),
+        })
     }
-    println!("{}", root.is_partial_word("aced".to_string()));
+
+    while window.is_open() && !window.is_key_down(Key::Enter) {
+        // render loop
+
+        window.update_with_buffer(&buffer, SIZE_X as usize, SIZE_Y as usize)?;
+    }
 
     Ok(())
 }
---
+++ src/vectors/vec2d.rs
@@ -0,0 +1,142 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+
+/*
+    Some simple vector utilites to kick it off
+*/
+#[derive(Copy, Clone, Debug, PartialEq, Default)]
+pub struct Vector2d {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Vector2d {
+    pub fn new(x: f64, y: f64) -> Vector2d {
+        Vector2d { x: x, y: y }
+    }
+
+    pub fn origin() -> Vector2d {
+        Vector2d { x: (0.0), y: (0.0) }
+    }
+
+    pub fn add(&mut self, v: &Vector2d) {
+        self.x += v.x;
+        self.y += v.y;
+    }
+
+    pub fn scale(&mut self, s: f64) {
+        self.x *= s;
+        self.y *= s;
+    }
+
+    pub fn rotate(&mut self, phi: f64) {
+        let x: f64 = self.x * f64::cos(phi) - self.y * f64::sin(phi);
+        let y: f64 = self.x * f64::sin(phi) + self.y * f64::cos(phi);
+
+        self.x = x;
+        self.y = y;
+    }
+
+    pub fn length(&self) -> f64 {
+        return ((self.x * self.x) + (self.y * self.y)).sqrt();
+    }
+}
+
+// v1 + v2
+impl Add for Vector2d {
+    type Output = Vector2d;
+
+    fn add(self, rhs: Vector2d) -> Vector2d {
+        Vector2d {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+// v1 += v2
+impl AddAssign for Vector2d {
+    fn add_assign(&mut self, rhs: Vector2d) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+// v1 - v2
+impl Sub for Vector2d {
+    type Output = Vector2d;
+
+    fn sub(self, rhs: Vector2d) -> Vector2d {
+        Vector2d {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+// v1 -= v2
+impl SubAssign for Vector2d {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+// v * scalar
+impl Mul<f64> for Vector2d {
+    type Output = Vector2d;
+
+    fn mul(self, rhs: f64) -> Vector2d {
+        Vector2d {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+// v *= scalar
+impl MulAssign<f64> for Vector2d {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+    }
+}
+
+// v / scalar
+impl Div<f64> for Vector2d {
+    type Output = Vector2d;
+
+    fn div(self, rhs: f64) -> Vector2d {
+        Vector2d {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+
+// v /= scalar
+impl DivAssign<f64> for Vector2d {
+    fn div_assign(&mut self, rhs: f64) {
+        self.x /= rhs;
+        self.y /= rhs;
+    }
+}
+
+// v * vector
+impl Mul<Vector2d> for Vector2d {
+    type Output = Vector2d;
+
+    fn mul(self, v: Vector2d) -> Vector2d {
+        Vector2d {
+            x: self.x * v.x,
+            y: self.y * v.y,
+        }
+    }
+}
+
+// v *= vector
+impl MulAssign<Vector2d> for Vector2d {
+    fn mul_assign(&mut self, v: Vector2d) {
+        self.x *= v.x;
+        self.y *= v.y;
+    }
+}
