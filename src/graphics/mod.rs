02980de77774d77aef8659a2332e4becb4e5eb0d
J <133714506+J-Feltens@users.noreply.github.com>
2025-11-23 22:48:39 +0100

    moved to rustrover, reformatted code a bit, moved cube to external function

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -6,7 +6,7 @@ pub mod triangles;
 
 // Re-export so callers can write `sprites::Sprite` directly
 pub use canvas::Canvas;
-pub use colors::{Color, WHITE, alpha_blend, rgb2u32};
+pub use colors::{Color, WHITE, alpha_blend};
 //noinspection RsUnusedImport
 pub use shapes::Cube;
 pub use triangles::Triangle3d;
